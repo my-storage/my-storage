1b7e720f91a689d8ba3c5dfc1ed2ec828d8a4468
Dhaniel Sales <dhanielr94@gmail.com>
2022-10-26 22:43:23 -0300

    feat: updates GetCurrentPath to not consider fix folder name

--- ms-profile/src/shared/utils/get_current_path.go
+++ ms-profile/src/shared/utils/get_current_path.go
@@ -8,7 +8,7 @@ import (
 func GetCurrentPath() (*string, error) {
 	dir, _ := os.Getwd()
 
-	path, err := filepath.Abs(filepath.Join(dir, "ms-profile"))
+	path, err := filepath.Abs(filepath.Join(dir))
 	if err != nil {
 		return nil, err
 	}
